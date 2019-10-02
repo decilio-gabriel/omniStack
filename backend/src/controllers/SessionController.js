caa87d3e8c18ba16f696a2cfdd298f7442d3f5f6
Gabriel Decilio <gabrieldecilio@iMac-de-Gabriel.local>
2019-10-02 11:58:56 -0300

    finalizacao

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -5,12 +5,11 @@ module.exports = {
   async store (req, res) {
     const { email } = req.body;
 
-    let user = await User.findOne({ email })
+    let user = await User.findOne({ email });
 
     if (!user) {
      const user = await User.create({ email });
     }
-
 
     return res.json(user);
   }
